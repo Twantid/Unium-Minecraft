8168798bce9acec15624bc12ef0ac275e4571300
Twantid <58114137+Twantid@users.noreply.github.com>
2019-12-14 19:13:20 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+myList = [5,3,5,2,4,3,4,5,4,2]
+print(myList)
+
+myList.append(4)
+print(myList)
+
+myList.extend("5")
+print(myList)
+
+myList.remove(4)
+print(myList)
+
+myList.pop(3)
+print(myList)
+
+print(myList.index(5))
+
+myList.count('2')
+print(myList)
+
+              
+
